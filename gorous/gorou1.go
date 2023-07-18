f8ebee5e52a4ba3fdc9c4f8be1dea752824058be
Saitama-97 <2822863494@qq.com>
2023-07-18 11:41:49 +0800

    Go-Chap6-07-18 git language test

---
+++ gorous/gorou1.go
@@ -0,0 +1,10 @@
+package gorous
+
+/**
+ * @Time    : 2023/7/18 11:40
+ * @File    : gorou1.go
+ * @Project : Chapter6
+ * @Author  : Saitama
+ * @IDE     : GoLand
+ * @Desc    : This is a test
+ */
